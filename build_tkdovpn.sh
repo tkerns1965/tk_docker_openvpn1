ff1cd9ee5a94ef98794e9b823225c060f64c1f8c
tkerns1965 <tony.kerns.65@gmail.com>
2017-10-02 11:33:10 -0400

    Update build_tkdovpn.sh

--- build_tkdovpn.sh
+++ build_tkdovpn.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker volume create vol_tkdovpn_certs
-cd ./base/
-docker build -t img_tkdovpn_base .
+# docker volume create vol_tkdovpn_certs
+cd ./easyrsa/
+docker build -t img_tkdovpn1_easyrsa .
 cd ../server/
-docker build -t img_tkdovpn_server .
+docker build -t img_tkdovpn1_server .
 cd ../client/
-docker build -t img_tkdovpn_client .
+docker build -t img_tkdovpn1_client .
 cd ../
