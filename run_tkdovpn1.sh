1ba0f9a3f3e08c0aa22351719604c98df3cc332d
tkerns1965 <tony.kerns.65@gmail.com>
2017-10-02 16:17:30 -0400

    Update run_tkdovpn1.sh

--- run_tkdovpn1.sh
+++ run_tkdovpn1.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+PASS_PHRASE=$1
+COMMON_NAME=$2
+
+if [ -z "$PASS_PHRASE" ]; then
+  echo "No pass phrase specified."
+  exit 1
+elif [ -z "$COMMON_NAME" ]; then
+  echo "No common name specified."
+  exit 1
+fi
+
 docker run --name cnt_tkdovpn1_easyrsa --mount src=vol_tkdovpn1_data,dst=/tkdovpn1/data \
-  img_tkdovpn1_easyrsa ./build_ca.exp
+  img_tkdovpn1_easyrsa ./build_ca.exp $PASS_PHRASE $COMMON_NAME
