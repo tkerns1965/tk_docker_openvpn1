402305eb72790b87340f1a032763f07dcc038bd5
tkerns1965 <tony.kerns.65@gmail.com>
2017-10-03 11:47:18 -0400

    create easyrsa_install_sign_client_req.sh

---
+++ easyrsa_install_sign_client_req.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+COMMON_NAME=$1
+PASS_PHRASE=$2
+
+if [ -z "$COMMON_NAME" ]; then
+  echo "No common name specified."
+  exit 1
+elif [ -z "$PASS_PHRASE" ]; then
+  echo "No pass phrase specified."
+  exit 1
+fi
+
+docker run --mount src=vol_ca_pki,dst=/etc/easyrsa/pki --mount src=vol_server_pki,dst=/etc/easyrsa/pki --rm \
+  img_tkdovpn1_easyrsa ./install_sign_server_req.exp $COMMON_NAME $PASS_PHRASE
