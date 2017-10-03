9d3f5b5c3d9b3b69870fd504a6b2056232012fde
tkerns1965 <tony.kerns.65@gmail.com>
2017-10-03 11:49:47 -0400

    Update easyrsa_build_ca.sh

--- easyrsa_build_ca.sh
+++ easyrsa_build_ca.sh
@@ -11,5 +11,5 @@ elif [ -z "$PASS_PHRASE" ]; then
   exit 1
 fi
 
-docker run --name cnt_tkdovpn1_easyrsa --mount src=vol_ca_pki,dst=/etc/easyrsa/pki --rm \
+docker run --mount src=vol_ca_pki,dst=/etc/easyrsa/pki --rm \
   img_tkdovpn1_easyrsa ./build_ca.exp $COMMON_NAME $PASS_PHRASE
