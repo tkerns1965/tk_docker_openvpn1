302669b3297e49b626d59215ca7cd127b5e567eb
tkerns1965 <tony.kerns.65@gmail.com>
2017-10-04 06:59:52 -0400

    Update start_tkdovpn1.sh

--- start_tkdovpn1.sh
+++ start_tkdovpn1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker run --name openvpn_svr --cap-add=NET_ADMIN --device=/dev/net/tun \
-  -d -p 1194:1194/udp img_tkdovpn1_server
+  --network net_tkdovpn1_bridge -d -p 1194:1194/udp img_tkdovpn1_server
 docker run --name openvpn_clt1 --cap-add=NET_ADMIN --device=/dev/net/tun \
-  -d img_tkdovpn1_client1
+  --network net_tkdovpn1_bridge -d img_tkdovpn1_client1
