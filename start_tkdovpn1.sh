45e345c13ee4c672d411c6606dd370e6cf349721
tkerns1965 <tony.kerns.65@gmail.com>
2017-10-04 12:30:36 -0400

    Update start_tkdovpn1.sh

--- start_tkdovpn1.sh
+++ start_tkdovpn1.sh
@@ -3,11 +3,10 @@
 docker run -d --name openvpn_svr --network net_tkdovpn1_bridge \
   --mount src=vol_server_pki,dst=/etc/easyrsa/pki \
   --cap-add=NET_ADMIN --device=/dev/net/tun -p 1194:1194/udp \
-  --entrypoint "openvpn --cd /etc/openvpn/ --config server.conf" \
-  img_tkdovpn1_server
+  img_tkdovpn1_server ./run_server.exp
 
-docker run -d --name openvpn_clt1 --network net_tkdovpn1_bridge \
-  --mount src=vol_client_pki,dst=/etc/easyrsa/pki \
-  --cap-add=NET_ADMIN --device=/dev/net/tun \
-  --entrypoint "openvpn --cd /etc/openvpn/ --config client.conf" \
-  img_tkdovpn1_client
+# docker run -d --name openvpn_clt1 --network net_tkdovpn1_bridge \
+  # --mount src=vol_client_pki,dst=/etc/easyrsa/pki \
+  # --cap-add=NET_ADMIN --device=/dev/net/tun \
+  # --entrypoint "openvpn --cd /etc/openvpn/ --config client.conf" \
+  # img_tkdovpn1_client
